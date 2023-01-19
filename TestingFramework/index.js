0157e22d7c487c925e46ba23fdc6aca6776525d5
Alan Martín López Chávez <alan.m.l.ch.1997@gmail.com>
2023-01-19 01:51:47 -0600

    test runner

--- TestingFramework/index.js
+++ TestingFramework/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("running test")
+const Runner = require("./runner");
+const runner = new Runner();
+
+const run = async () =>{
+    await runner.collectFiles(process.cwd());
+    console.log(runner.testFiles)
+}
+
+run();
+// console.log("running test")
